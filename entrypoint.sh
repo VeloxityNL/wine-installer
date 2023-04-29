d5fe96db9e2ad09b1ab97d4fc71b291dac6fc1b6
VeloxityNL <84510550+VeloxityNL@users.noreply.github.com>
2023-04-29 21:33:44 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 cd /home/container
 
-# Wine Versie
+# Output Current Wine Version
 wine --version
 
-# Als .wine niet bestaat, kopieer de backup
-if [ ! -d /home/container/.wine ];
-then echo "Wineprefix is not found, initializing wine" && winecfg && /usr/sbin/winetricks
+# If .wine directory doesn't exist, copy backup
+if [ ! -d /home/container/.wine ]; 
+then echo "Wineprefix not found, initialiizing wine" && winecfg && /usr/sbin/winetricks
 echo "Configured Succesfully"
-
 fi;
 
-# Vervangt de startip variabelen
+# Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":home/container$ ${MODIDIEF_STARTUP}"
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Start de server
-${MODIDIEF_STARTUP}
+# Run the Server
+${MODIFIED_STARTUP}
